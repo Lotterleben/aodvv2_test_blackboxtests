20343fcaf54e9fc41ca34fccd01c3b6d4a24c40d
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2015-06-29 07:53:28 -0700

    fix all the testings

--- aodvv2_blackbox_tests.go
+++ aodvv2_blackbox_tests.go
@@ -5,26 +5,29 @@ import (
     "fmt"
 )
 
+/* route states */
+const
+(
+    ROUTE_STATE_ACTIVE = iota
+    ROUTE_STATE_IDLE = iota
+    ROUTE_STATE_INVALID = iota
+    ROUTE_STATE_TIMED = iota
+)
+
+const test_string = "xoxotesttest"
+const json_template_sent_rreq = "{\"log_type\": \"sent_rreq\", \"log_data\": {\"orig_addr\": \"%s\", \"targ_addr\": \"%s\", \"seqnum\": %d, \"metric\": %d}}"
+const json_template_received_rreq = "{\"log_type\": \"received_rreq\", \"log_data\":{\"last_hop\": \"%s\", \"orig_addr\": \"%s\", \"targ_addr\": \"%s\", \"orig_addr_seqnum\": %d, \"metric\": %d}}"
+const json_template_sent_rrep = "{\"log_type\": \"sent_rrep\", \"log_data\": {\"next_hop\": \"%s\",\"orig_addr\": \"%s\", \"orig_addr_seqnum\": %d, \"targ_addr\": \"%s\"}}"
+const json_template_received_rrep = "{\"log_type\": \"received_rrep\", \"log_data\":{\"last_hop\": \"%s\", \"orig_addr\": \"%s\", \"orig_addr_seqnum\": %d, \"targ_addr\": \"%s\"}}"
+const json_template_added_rt_entry = "{\"log_type\": \"added_rt_entry\", \"log_data\": {\"addr\": \"%s\", \"next_hop\": \"%s\", \"seqnum\": %d, \"metric\": %d, \"state\": %d}}"
+
+
 func test_route_creation_0_to_3() {
-    /* route states */
-    const
-    (
-        ROUTE_STATE_ACTIVE = iota
-        ROUTE_STATE_IDLE = iota
-        ROUTE_STATE_INVALID = iota
-        ROUTE_STATE_TIMED = iota
-    )
+    test_name := "route_creation_0_to_3"
 
-    const test_string = "xoxotesttest"
-    const json_template_sent_rreq = "{\"log_type\": \"sent_rreq\", \"log_data\": {\"orig_addr\": \"%s\", \"targ_addr\": \"%s\", \"seqnum\": %d, \"metric\": %d}}"
-    const json_template_received_rreq = "{\"log_type\": \"received_rreq\", \"log_data\":{\"last_hop\": \"%s\", \"orig_addr\": \"%s\", \"targ_addr\": \"%s\", \"orig_addr_seqnum\": %d, \"metric\": %d}}"
-    const json_template_sent_rrep = "{\"log_type\": \"sent_rrep\", \"log_data\": {\"next_hop\": \"%s\",\"orig_addr\": \"%s\", \"orig_addr_seqnum\": %d, \"targ_addr\": \"%s\"}}"
-    const json_template_received_rrep = "{\"log_type\": \"received_rrep\", \"log_data\":{\"last_hop\": \"%s\", \"orig_addr\": \"%s\", \"orig_addr_seqnum\": %d, \"targ_addr\": \"%s\"}}"
-    const json_template_added_rt_entry = "{\"log_type\": \"added_rt_entry\", \"log_data\": {\"addr\": \"%s\", \"next_hop\": \"%s\", \"seqnum\": %d, \"metric\": %d, \"state\": %d}}"
+    riot_line := aodvv2_test_management.Create_clean_setup(test_name)
 
-    riot_line := aodvv2_test_management.Create_clean_setup("testtest")
-
-    fmt.Println("Starting test...")
+    fmt.Println("Starting test ", test_name)
 
     beginning := riot_line[0]
     end := riot_line[len(riot_line)-1]
@@ -33,71 +36,67 @@ func test_route_creation_0_to_3() {
     fmt.Print(".")
 
     /* Discover route...  */
-
-    /*
-    expected_json := fmt.Sprintf(json_template_sent_rreq, beginning.ip, end.ip, 1, 0)
+    expected_json := fmt.Sprintf(json_template_sent_rreq, beginning.Ip, end.Ip, 1, 0)
     fmt.Print(".")
-    beginning.channels.expect_JSON(expected_json)
+    beginning.Channels.Expect_JSON(expected_json)
     fmt.Print(".")
 
-    expected_json = fmt.Sprintf(json_template_received_rreq, beginning.ip, beginning.ip, end.ip, 1, 0)
-    riot_line[1].channels.expect_JSON(expected_json)
+    expected_json = fmt.Sprintf(json_template_received_rreq, beginning.Ip, beginning.Ip, end.Ip, 1, 0)
+    riot_line[1].Channels.Expect_JSON(expected_json)
     fmt.Print(".")
-    expected_json = fmt.Sprintf(json_template_added_rt_entry, beginning.ip, beginning.ip, 1, 1, ROUTE_STATE_ACTIVE)
-    riot_line[1].channels.expect_JSON(expected_json)
+    expected_json = fmt.Sprintf(json_template_added_rt_entry, beginning.Ip, beginning.Ip, 1, 1, ROUTE_STATE_ACTIVE)
+    riot_line[1].Channels.Expect_JSON(expected_json)
     fmt.Print(".")
-    expected_json = fmt.Sprintf(json_template_sent_rreq, beginning.ip, end.ip, 1, 1)
-    riot_line[1].channels.expect_JSON(expected_json)
-    fmt.Print(".")
-
-    expected_json = fmt.Sprintf(json_template_received_rreq, riot_line[1].ip, beginning.ip, end.ip, 1, 1)
-    riot_line[2].channels.expect_JSON(expected_json)
-    fmt.Print(".")
-    expected_json = fmt.Sprintf(json_template_added_rt_entry, beginning.ip, riot_line[1].ip, 1, 2, ROUTE_STATE_ACTIVE)
-    riot_line[2].channels.expect_JSON(expected_json)
-    fmt.Print(".")
-    expected_json = fmt.Sprintf(json_template_sent_rreq, beginning.ip, end.ip, 1, 2)
-    riot_line[2].channels.expect_JSON(expected_json)
+    expected_json = fmt.Sprintf(json_template_sent_rreq, beginning.Ip, end.Ip, 1, 1)
+    riot_line[1].Channels.Expect_JSON(expected_json)
     fmt.Print(".")
 
-    expected_json = fmt.Sprintf(json_template_received_rreq, riot_line[2].ip, beginning.ip, end.ip, 1, 2)
-    end.channels.expect_JSON(expected_json)
+    expected_json = fmt.Sprintf(json_template_received_rreq, riot_line[1].Ip, beginning.Ip, end.Ip, 1, 1)
+    riot_line[2].Channels.Expect_JSON(expected_json)
     fmt.Print(".")
-    expected_json = fmt.Sprintf(json_template_added_rt_entry, beginning.ip, riot_line[2].ip, 1, 3, ROUTE_STATE_ACTIVE)
-    end.channels.expect_JSON(expected_json)
+    expected_json = fmt.Sprintf(json_template_added_rt_entry, beginning.Ip, riot_line[1].Ip, 1, 2, ROUTE_STATE_ACTIVE)
+    riot_line[2].Channels.Expect_JSON(expected_json)
     fmt.Print(".")
-    */
+    expected_json = fmt.Sprintf(json_template_sent_rreq, beginning.Ip, end.Ip, 1, 2)
+    riot_line[2].Channels.Expect_JSON(expected_json)
+    fmt.Print(".")
+
+    expected_json = fmt.Sprintf(json_template_received_rreq, riot_line[2].Ip, beginning.Ip, end.Ip, 1, 2)
+    end.Channels.Expect_JSON(expected_json)
+    fmt.Print(".")
+    expected_json = fmt.Sprintf(json_template_added_rt_entry, beginning.Ip, riot_line[2].Ip, 1, 3, ROUTE_STATE_ACTIVE)
+    end.Channels.Expect_JSON(expected_json)
+    fmt.Print(".")
+
     /* And send a RREP back */
     /* NOTE: added_rt_entry isn't checked on the was back yet because apparently
      * weird RREQs are sent out before the experiment, screwing up the targaddr seqnum
      * and I haven't figured out why yet. TODO FIXME */
 
-    /*
-    expected_json = fmt.Sprintf(json_template_sent_rrep, riot_line[2].ip, beginning.ip, 1, end.ip)
-    end.channels.expect_JSON(expected_json)
+    expected_json = fmt.Sprintf(json_template_sent_rrep, riot_line[2].Ip, beginning.Ip, 1, end.Ip)
+    end.Channels.Expect_JSON(expected_json)
     fmt.Print(".")
 
-    expected_json= fmt.Sprintf(json_template_received_rrep, end.ip, beginning.ip, 1, end.ip)
-    riot_line[2].channels.expect_JSON(expected_json)
+    expected_json= fmt.Sprintf(json_template_received_rrep, end.Ip, beginning.Ip, 1, end.Ip)
+    riot_line[2].Channels.Expect_JSON(expected_json)
     fmt.Print(".")
-    expected_json= fmt.Sprintf(json_template_sent_rrep, riot_line[1].ip, beginning.ip, 1, end.ip)
-    riot_line[2].channels.expect_JSON(expected_json)
-    fmt.Print(".")
-
-    expected_json= fmt.Sprintf(json_template_received_rrep, riot_line[2].ip, beginning.ip, 1, end.ip)
-    riot_line[1].channels.expect_JSON(expected_json)
-    fmt.Print(".")
-    expected_json= fmt.Sprintf(json_template_sent_rrep, beginning.ip, beginning.ip, 1, end.ip)
-    riot_line[1].channels.expect_JSON(expected_json)
+    expected_json= fmt.Sprintf(json_template_sent_rrep, riot_line[1].Ip, beginning.Ip, 1, end.Ip)
+    riot_line[2].Channels.Expect_JSON(expected_json)
     fmt.Print(".")
 
-    expected_json= fmt.Sprintf(json_template_received_rrep, riot_line[1].ip, beginning.ip, 1, end.ip)
-    beginning.channels.expect_JSON(expected_json)
+    expected_json= fmt.Sprintf(json_template_received_rrep, riot_line[2].Ip, beginning.Ip, 1, end.Ip)
+    riot_line[1].Channels.Expect_JSON(expected_json)
+    fmt.Print(".")
+    expected_json= fmt.Sprintf(json_template_sent_rrep, beginning.Ip, beginning.Ip, 1, end.Ip)
+    riot_line[1].Channels.Expect_JSON(expected_json)
     fmt.Print(".")
 
-    //TODO: defer dump channels
+    expected_json= fmt.Sprintf(json_template_received_rrep, riot_line[1].Ip, beginning.Ip, 1, end.Ip)
+    beginning.Channels.Expect_JSON(expected_json)
+    fmt.Print(".")
+
+    //TODO: defer dump Channels
     fmt.Println("\nDone.")
-    */
 }
 
 func start_experiments() {
